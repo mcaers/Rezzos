9d2dd58183eefe23f73d73ae670b7cf1aaa187bf
Michelle Caers <mcaers@gmail.com>
2013-03-27 11:32:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rezzos::Application.config.secret_token = 'f0fb2f643f9e7e1fc89f1fba52bafdba6a35039f9dbbe45735e11d5c3599d09b74c0c863bb96202f53446cd2a06f921e1a9ad9c898d3517f151a1f95c0232d71'
